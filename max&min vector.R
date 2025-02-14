cce064da4b34ee82c3186d5a085393dea3cbfbf5
shanmukhi22 <192324315l.simats@saveetha.com>
2025-02-14 09:50:31 +0530

    Add files via upload

---
+++ max&min vector.R
@@ -0,0 +1,7 @@
+vec <- c(12, 45, 7, 89, 23, 56, 3, 91, 17, 32)
+max_val <- max(vec)
+min_val <- min(vec)
+cat("Vector:", vec, "\n")
+cat("Maximum value:", max_val, "\n")
+cat("Minimum value:", min_val, "\n")
+
