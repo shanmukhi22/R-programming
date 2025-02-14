c9322e0eea311839769415b62e210168bd67c11f
shanmukhi22 <192324315l.simats@saveetha.com>
2025-02-14 09:03:51 +0530

    Add files via upload

---
+++ seq of num.R
@@ -0,0 +1,8 @@
+seq_nums <- 20:50
+mean_val <- mean(20:60)
+sum_val <- sum(51:91)
+
+cat("Sequence from 20 to 50:", seq_nums, "\n")
+cat("Mean of numbers from 20 to 60:", mean_val, "\n")
+cat("Sum of numbers from 51 to 91:", sum_val, "\n")
+
